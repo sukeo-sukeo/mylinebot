1cda14ab848e75cdbde91d108605c28efbb4ef69
sukeo-sukeo <syuke4522@gmail.com>
2020-10-22 21:13:51 +0900

    sendMessage adjustment

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 const app = express();
+require('dotenv').config()
+
+const monsterData = require('./data')
+
 
 // create LINE SDK config from env variables
 const config = {
@@ -19,15 +23,14 @@ app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => {
-      const text = req.body.events[0].message.text
-
-      res.json(text)
+      res.json(result)
     })
     .catch((err) => {
       console.error(err);
       res.status(500).end();
     });
 });
+
 
 // event handler
 function handleEvent(event) {
@@ -38,13 +41,43 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const reMess = event.message.text
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  console.time('process-time');
+
+  const serchMonster = (monsterDatas, tage) => {
+    monsterDatas.some((monsterData, i) => {
+      const result = tage.indexOf(monsterData.name)
+      if (result === 0) {
+        index.push(i)
+        return true
+      }
+    })
+  }
+
+  console.timeEnd('process-time');
+
+  let index = []
+  serchMonster(monsterData, reMess)
+
+  //送信メッセージ
+  if(index[0] === undefined) {
+    const seMess = {
+      type: 'text',
+      text: 'そんなモンスターいないニャ！'
+    }
+    return client.replyMessage(event.replyToken, seMess);
+  } else {
+    const seMess = {
+      type: 'text',
+      text: '弱点は' + '「 ' + monsterData[index[0]].weak1 + ' 」' + ' ニャ'
+    }
+    return client.replyMessage(event.replyToken, seMess);
+  }
+
 }
+
 
 // listen on port
 const port = process.env.PORT || 3000;
